6db317ce2d590f321d32a101c0db52f968849351
Hui En <huien_wong@hotmail.com>
2025-04-19 17:56:39 +0800

    Set up query client and mock server

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import { server } from "src/api/mocks/server";
+
+server.listen({ onUnhandledRequest: 'bypass' });
 
 export default function RootLayout() {
+    const queryClient = new QueryClient();
+
     return (
-        <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="index" />
-            <Stack.Screen name="transfer" options={{ gestureEnabled: false }} />
-        </Stack>
+        <QueryClientProvider client={queryClient}>
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="index" />
+                <Stack.Screen name="transfer" options={{ gestureEnabled: false }} />
+            </Stack>
+        </QueryClientProvider>
     );
 }
